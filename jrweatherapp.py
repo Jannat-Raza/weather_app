6c432ace101ce5ace0aaf05a982495dca21f31b2
Jannat-Raza <108749549+Jannat-Raza@users.noreply.github.com>
2024-10-21 16:18:07 +0500

    Update jrweatherapp.py

--- jrweatherapp.py
+++ jrweatherapp.py
@@ -7,12 +7,20 @@ def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     return response.json()
-
 st.markdown(
     """
     <style>
     .stApp {
         background-color: black;
+        color: white;
+    }
+    .title {
+        color: white;
+    }
+    .subtitle {
+        color: white;
+    }
+    .weather-title {
         color: white;
     }
     </style>
